b6a505103ea8730fa6ee9723bbb6e6191fd44364
Nguyen Ba Cong <bacongso1nha@gmail.com>
2026-03-03 13:51:27 +0700

    Nop BTH2

---
+++ TH-3/main.js
@@ -0,0 +1,30 @@
+console.log("Hello from JavaScript!");
+let name = "Công";
+let yearOfBirth = 2006;
+let currentYear = 2026;
+let age = currentYear - yearOfBirth;
+
+console.log("Xin chào mình là " + name + ", năm nay mình " + age + "tuổi");
+let score = 8;
+
+// TODO: Dự đoán điều kiện if/else đang làm gì, rồi chạy thử
+if (score >= 8) {
+  console.log("Giỏi chà giỏi thế");
+} else if (score >= 6.5) {
+  console.log("Khá cố lên tí nữa là đc rồi");
+} else if (score >= 5) {
+  console.log("Trung bình hơi thấp nha");
+} else {
+  console.log("Yếu đi về học lại đi");
+}
+
+// TODO: Viết hàm tính điểm trung bình 3 môn
+function tinhDiemTrungBinh(m1, m2, m3) {
+  let avg = (m1 + m2 + m3) / 3;
+  return avg;
+}
+function xeoloai(avg) {
+  if (avg >= 8) {
+    print;
+  }
+}
